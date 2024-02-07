a13a9350cf9d417b5c61dec2b70efe970b17e5f1
isaquexavier1155 <isaque.ixs@gmail.com>
2024-02-07 17:36:18 -0300

    Ajuste página Listagem carros

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -256,7 +256,7 @@
                     <div class="get-in-touch-box d-flex mb-2">
                         <i class="flaticon-mail"></i>
                         <div class="media-body">
-                            <a href="#">contato@joaoautomoveisparobe.com.br</a>
+                            <!-- <a href="#">contato@joaoautomoveisparobe.com.br</a> -->
                         </div>
                     </div>
                     <div class="get-in-touch-box d-flex">
