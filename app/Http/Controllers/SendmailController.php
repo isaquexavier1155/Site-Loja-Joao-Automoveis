1970898a1757915a37b911d1e487bd060147e378
isaquexavier1155 <isaque.ixs@gmail.com>
2024-02-22 13:38:47 -0300

    Implemenação de funcionalidade para salvar dados do cliente ao enviar email na página contato

--- app/Http/Controllers/SendmailController.php
+++ app/Http/Controllers/SendmailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Mail\Sendmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Models\ContatoWhatsapp;
 
 class SendmailController extends Controller
 {
@@ -13,11 +14,19 @@ class SendmailController extends Controller
         $nome = $request->input('nome');
         $email = $request->input('email');
         $assunto = $request->input('assunto');
-        
         // Remova a máscara do telefone antes de atribuí-lo à variável
         $telefone = str_replace(['(', ')', ' ', '-'], '', $request->input('telefone'));
-        
         $mensagem = $request->input('mensagem');
+
+        // Criar um novo registro na tabela contato_whatsapp
+        $contato = new ContatoWhatsapp([
+            'nome' => $nome,
+            'ip' => "Contato via Email",
+            'email' => $email,
+            'telefone' => $telefone,
+            'data_contato' => now(),
+        ]);
+        $contato->save();
     
         $data = [
             'nome' => $nome,
